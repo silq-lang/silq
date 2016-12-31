8ff01bd1cfd022c41e9e43e7cd1e5b35cb29c6b6
Timon Gehr <timon.gehr@gmx.ch>
2016-12-31 23:04:06 +0100

    Update to DMD 2.072.2.

--- dependencies.sh
+++ dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ ! -d dmd2 ]; then
-    SUM1="cf9db5363e8fa97c4e5c139898abef71  dmd.2.072.1.linux.zip"
-    wget http://downloads.dlang.org/releases/2.x/2.072.1/dmd.2.072.1.linux.zip
-    SUM2=`md5sum dmd.2.072.1.linux.zip`
+    SUM1="c6dc5df0eeac35d37db167b9a80eb08e  dmd.2.072.2.linux.zip"
+    wget http://downloads.dlang.org/releases/2.x/2.072.2/dmd.2.072.2.linux.zip
+    SUM2=`md5sum dmd.2.072.2.linux.zip`
     if [[ $SUM1 != $SUM2 ]]; then
 	echo "ERROR: md5 sum mismatch"
     else
-	unzip dmd.2.072.1.linux.zip
+	unzip dmd.2.072.2.linux.zip
     fi
 fi
