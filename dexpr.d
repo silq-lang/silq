61154f828028656d8cb6ff00abff947b7f9db050
Timon Gehr <timon.gehr@gmx.ch>
2017-03-10 16:42:21 +0100

    Disable measuring code.

--- dexpr.d
+++ dexpr.d
@@ -25,13 +25,13 @@ struct RecursiveStopWatch{
 	auto peek(){ return sw.peek(); }
 }
 
-RecursiveStopWatch sw;
+/+RecursiveStopWatch sw;
 int swCount=0;
 static ~this(){
 	writeln("time: ",sw.peek().to!("seconds",double));
 	writeln("freq: ",swCount);
 }
-enum measure="swCount++;sw.start();scope(exit)sw.stop();";
+enum measure="swCount++;sw.start();scope(exit)sw.stop();";+/
 
 //version=DISABLE_INTEGRATION;
 
