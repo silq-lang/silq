0c6183d4154c79fd7e29cd43bebaf27313f73084
Timon Gehr <timon.gehr@gmx.ch>
2017-04-11 12:50:47 +0200

    Fix precondition check for continuous integration.

--- integration.d
+++ integration.d
@@ -163,6 +163,34 @@ private DExpr definiteIntegralImpl(DExpr expr,DExpr facts=one){
 	return null;
 }
 
+bool isContinuousMeasureIn(DExpr expr,DVar var){
+	if(!expr.hasFreeVar(var)) return true;
+	if(auto d=cast(DDistApply)expr) return !d.arg.hasFreeVar(var);
+	if(auto d=cast(DDelta)expr) return !d.e.hasFreeVar(var);
+	if(auto d=cast(DDiscDelta)expr) return !d.var.hasFreeVar(var);
+	if(cast(DIvr)expr||cast(DVar)expr||cast(DPow)expr||cast(DLog)expr||cast(DSin)expr||cast(DFloor)expr||cast(DCeil)expr||cast(DGaussInt)expr||cast(DAbs)expr)
+		return true;
+	if(auto p=cast(DPlus)expr){
+		foreach(s;p.summands)
+			if(!isContinuousMeasureIn(s,var))
+				return false;
+		return true;
+	}
+	if(auto m=cast(DMult)expr){
+		foreach(f;m.factors)
+			if(!isContinuousMeasureIn(f,var))
+				return false;
+		return true;
+	}
+	if(auto i=cast(DInt)expr) return isContinuousMeasureIn(i.expr,var.incDeBruijnVar(1,0));
+	if(auto c=cast(DMCase)expr){
+		if(!isContinuousMeasureIn(c.val,var.incDeBruijnVar(1,0))) return false;
+		return isContinuousMeasureIn(c.err,var);
+	}
+	return false;
+}
+
+
 private DExpr definiteIntegralContinuous(DExpr expr,DExpr facts)out(res){
 	version(INTEGRATION_STATS){
 		integrations++;
@@ -171,12 +199,7 @@ private DExpr definiteIntegralContinuous(DExpr expr,DExpr facts)out(res){
 }body{
 	auto var=dDeBruijnVar(1);
 	// ensure integral is continuous
-	foreach(f;expr.allOf!DDistApply(true))
-		if(f.hasFreeVar(var)) return null;
-	foreach(d;expr.allOf!DDelta(true))
-		if(d.hasFreeVar(var)) return null;
-	foreach(d;expr.allOf!DDiscDelta(true))
-		if(d.hasFreeVar(var)) return null;
+	if(!expr.isContinuousMeasureIn(var)) return null;
 
 	assert(expr.factors.all!(x=>x.hasFreeVar(var)));
 	assert(expr.factors.all!(x=>!cast(DDelta)x));
