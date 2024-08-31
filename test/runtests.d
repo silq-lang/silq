39eed011a4130dd477d9b506a91ecd6558c2ffcb
Timon Gehr <timon.gehr@gmx.ch>
2024-08-31 02:02:44 +0200

    Fix.

--- test/runtests.d
+++ test/runtests.d
@@ -110,10 +110,10 @@ int main(string[] args){
 				if(colorize) writef(" % 5.0fms",time.to!("msecs",double));
 				else writef(" in %.0fms",time.to!("msecs",double));
 			}
-			totalTime+=time;
 			if(colorize) writeln(" ",source,CLEAR_LINE);
 			else writeln();
 		}else if(colorize) std.stdio.write("\r",CLEAR_LINE);
+		totalTime+=time;
 	}
 	writeln();
 	if(colorize) writeln(BOLD,"TOTAL:",RESET," ",sources.length);
