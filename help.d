bd5b21675aa51227310eef25dbefb9f9117690cf
Timon Gehr <timon.gehr@gmx.ch>
2025-10-01 15:38:43 +0200

    Document some command-line options.

--- help.d
+++ help.d
@@ -10,13 +10,20 @@ enum help=`Silq type checker and simulator
 Usage: silq [OPTION]... [FILE]...
 
 The options below may be used.
---run               run main function in simulator
---trace             print statements as they run together with the program state
+--run                run main function in simulator
+--run-on=exp         run main function on argument given by exp
+--run-on-each=exp    run main function on each element of exp
+--run=exp            run expression exp
+--repeat=n           repeat the run n times
+--trace              print statements as they run together with the program state
+--seed=n             use a fixed random seed for repeatable experiments
 
---summarize=...     summarize function declarations and exit (ex: --summarize=[name,arg-arity,ret-arity])
---error-json        print diagnostics in json format
+--inference-limit=n  use at most n iterations for inferring fixed points
 
---help              display this help and exit
+--summarize=...      summarize function declarations and exit (ex: --summarize=[name,arg-arity,ret-arity])
+--error-json         print diagnostics in json format
+
+--help               display this help and exit
 
 Recognized file extensions: *.slq
 
